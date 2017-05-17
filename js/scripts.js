90b6ffb5ba8a25c92211d79b89ff5d0c968814ee
Patryk <patnow92@wp.pl>
2017-05-17 16:51:10 +0200

    add files

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var maleNames = ['Patryk' , 'Michał' , 'Tomasz' , 'Łukasz'];
+var femaleNames = ['Viola' , 'Kasia' , 'Ania' , 'Magda'];
+var allNames = maleNames.concat(femaleNames);
+var newName = 'Marian'
+
+console.log(allNames);
+
+if(allNames.indexOf(newName) === -1)
+{
+  allNames.push(newName);
+}
+else {
+  console.log(allNames);
+}
