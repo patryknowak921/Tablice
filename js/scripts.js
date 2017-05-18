eb913ef6d6223ca3082409458983e73d8bd64f59
Patryk <patnow92@wp.pl>
2017-05-18 15:54:43 +0200

    poprawione

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
 var maleNames = ['Patryk' , 'Michał' , 'Tomasz' , 'Łukasz'];
 var femaleNames = ['Viola' , 'Kasia' , 'Ania' , 'Magda'];
 var allNames = maleNames.concat(femaleNames);
-var newName = 'Marian'
+var newName = 'Marian';
 
 console.log(allNames);
 
 if(allNames.indexOf(newName) === -1)
-{
-  allNames.push(newName);
-}
-else {
+	allNames.push(newName); {
+
+} else {
   console.log(allNames);
 }
